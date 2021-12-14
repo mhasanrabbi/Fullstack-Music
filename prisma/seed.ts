94627e9ece4c99754fff4104458e92cac689842b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-12-14 21:51:47 +0600

    seed-done

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,6 +23,7 @@ const run = async () => {
       });
     })
   );
+
   const salt = bcrypt.genSaltSync();
   const user = await prisma.user.upsert({
     where: { email: "user@test.com" },
@@ -32,6 +33,25 @@ const run = async () => {
       password: bcrypt.hashSync("password", salt),
     },
   });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
@@ -39,7 +59,6 @@ run()
     console.error(e);
     process.exit(1);
   })
-  .then()
   .finally(async () => {
     await prisma.$disconnect();
   });
