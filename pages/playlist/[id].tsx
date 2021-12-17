ddd03fe631f52a8dca2b3f28ff7c3683b02d1c6f
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-12-17 12:31:16 +0600

    fix auth server side

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -38,11 +38,19 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      permanent: false,
+      path: "/signin",
+    };
+  }
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
